cd122d5e557498f2f6a2cee7c6ee66f3d7b1ef27
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2018-01-14 15:45:48 +0000

    ignore object modifications (breaks signals, decreases overhead)

--- lachesis/models/database.py
+++ lachesis/models/database.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from lachesis import app, settings
 
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
 
 db = SQLAlchemy(app)
 
--- lachesis/settings.py
+++ lachesis/settings.py
@@ -2,3 +2,4 @@ import os
 
 
 DATABASE_URL = os.getenv('DATABASE_URL') if 'DATABASE_URL' in os.environ else 'sqlite://'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
