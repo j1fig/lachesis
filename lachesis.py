4b626d8295dc98eb303f37397c40d873297d9463
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2018-01-14 11:45:37 +0000

    added first failing tests

---
+++ lachesis.py
@@ -0,0 +1,37 @@
+import json
+import pickle
+import pandas as pd
+from flask import Flask, request, jsonify
+
+
+def reload_classifier():
+    with open('columns.json') as fh:
+        columns = json.load(fh)
+
+    with open('classifier.pickle', 'rb') as fh:
+        classifier = pickle.load(fh)
+
+    with open('dtypes.pickle', 'rb') as fh:
+        dtypes = pickle.load(fh)
+
+    return classifier, columns, dtypes
+
+
+app = Flask(__name__)
+
+
+classifier, columns, dtypes = reload_classifier()
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    payload = request.get_json()
+    obs = pd.DataFrame([payload], columns=columns).astype(dtypes)
+    proba = classifier.predict_proba(obs)[0, 1]
+    return jsonify({
+        'prediction': proba
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ lachesis_test.py
@@ -0,0 +1,74 @@
+import http
+import json
+import unittest
+
+import lachesis
+
+
+class LachesisTest(unittest.TestCase):
+
+    def setUp(self):
+        lachesis.app.testing = True
+        self.c = lachesis.app.test_client()
+        # with lachesis.app.app_context():
+        #     lachesis.init_db()
+        self.good_observation = {
+            "id": 1,
+            "observation": {
+                "birth date": "1983-12-26",
+                "job type": "private",
+                "school level": "secondary",
+                "domestic status": "single",
+                "profession": "mechanic",
+                "domestic relationship type": "never married",
+                "ethnicity": "afro american",
+                "gender": "Female",
+                "earned dividends": 0,
+                "interest earned": 0,
+                "monthly work": 160,
+                "country of origin": "u.s.",
+                "target": 1
+            }
+        }
+
+    def _public_api_request(self, method, url, *args, **kwargs):
+        """
+        Perform an API request.
+        :param method: Method to use ('GET', 'POST' etc).
+        :param url: Relative URL.
+        """
+        kwargs.setdefault('headers', {})
+        kwargs['method'] = method
+        res = self.c.open(url, *args, **kwargs)
+        return res
+
+    def _public_api_json_request(self, method, url, data, *args, **kwargs):
+        """
+        Perform an HTTP request with a JSON body payload.
+        :param method: Method to use ('POST', 'PUT' etc).
+        :param url: Relative URL.
+        :param data: Dictionary containing data to send.
+        """
+        res = self._public_api_request(
+            method,
+            url,
+            data=json.dumps(data),
+            content_type='application/json',
+            *args,
+            **kwargs
+        )
+        return res
+
+    def test_predict_health(self):
+        r = self._public_api_json_request('POST', '/predict', self.good_observation)
+        self.assertEqual(http.OK, r.status_code)
+
+    def test_predict_sanity(self):
+        r = self._public_api_json_request('POST', '/predict', self.good_observation)
+        self.assertEqual(
+            json.loads(r.data),
+            {'prediction': 0.5}
+        )
+
+if __name__ == '__main__':
+    unittest.main()
